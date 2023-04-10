25c4f724fc7b0133156a574871e731b71e94bab7
Viktor1504 <viktordenisevich6@gmail.com>
2023-04-11 00:06:04 +0300

    FirstTask

--- script-if-cycle.js
+++ script-if-cycle.js
@@ -1,16 +1,6 @@
 'use strict'
 
-let result = '';
-const length = 7;
-
-for (let i = 0; i < length; i++) {
-
-    for (let j = 0; j < i; j++) {
-        result += '*';
-    }
-
-    result += '\n';
+for (let i = 5; i < 11 ; i++) {
+    console.log(i);
 }
-
-console.log(result);
 
