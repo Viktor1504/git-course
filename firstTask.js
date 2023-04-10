e524851eb59685eeb92e20d368953da72bbd29e0
Viktor1504 <viktordenisevich6@gmail.com>
2023-04-11 00:16:25 +0300

    firstTask

---
+++ firstTask.js
@@ -0,0 +1,5 @@
+'use strict'
+
+for (let i = 5; i < 10; i++) {
+    console.log(i);
+}
--- script-if-cycle.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-for (let i = 20; i >= 10; i--) {
-    console.log(i);
-    if (i === 13) break;
-}
