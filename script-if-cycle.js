f79a8a14364345d4500c6c04a2a4cca5fdbd9887
Viktor1504 <viktordenisevich6@gmail.com>
2023-04-10 23:59:18 +0300

    triangle nesting

--- script-if-cycle.js
+++ script-if-cycle.js
@@ -4,6 +4,13 @@ let result = '';
 const length = 7;
 
 for (let i = 0; i < length; i++) {
-    result += '*';
-    console.log(result);
+
+    for (let j = 0; j < i; j++) {
+        result += '*';
+    }
+
+    result += '\n';
 }
+
+console.log(result);
+
