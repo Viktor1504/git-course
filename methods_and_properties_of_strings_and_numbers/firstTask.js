c8c28f1c9fa8b4daef6b0afeaf0c75da947c934b
Viktor1504 <viktordenisevich6@gmail.com>
2023-04-18 00:35:40 +0300

    firstTask

---
+++ methods_and_properties_of_strings_and_numbers/firstTask.js
@@ -0,0 +1,59 @@
+'use strict'
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+function rememberMyFilms() {
+    for (let i = 0; i < numberOfFilms; i++) {
+        const recentlyViewedMovie = prompt('Один из последних просмотренных фильмов', '');
+        const movieRating = +prompt('На сколько оцените его?', '');
+        if (recentlyViewedMovie != null && movieRating != null && recentlyViewedMovie !== '' && movieRating !== '' && recentlyViewedMovie.length < 50) {
+            console.log('done');
+            personalMovieDB.movies[recentlyViewedMovie] = movieRating;
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+
+rememberMyFilms();
+
+function detectPersonalMovie() {
+    if (personalMovieDB.count < 10) {
+        alert('Просмотренно довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+        alert('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        alert('Вы киноман');
+    } else {
+        alert('Произошла ошибка');
+    }
+}
+
+detectPersonalMovie();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB)
+    }
+}
+
+showMyDB(personalMovieDB.private);
