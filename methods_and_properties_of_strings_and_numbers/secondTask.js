07baf404ef4ad1e9dec8d9ea91666ba60c70d4d8
Viktor1504 <viktordenisevich6@gmail.com>
2023-04-19 00:29:05 +0300

    thirdTask

--- methods_and_properties_of_strings_and_numbers/secondTask.js
+++ methods_and_properties_of_strings_and_numbers/secondTask.js
@@ -1,9 +1,17 @@
 'use strict'
 
-function calculateVolumeAndArea(a) {
-    const v = a * a * a;
-    const s = 6 * (a * a);
-    return `Объем куба: ${v}, площадь всей поверхности: ${s}`;
+function calculateVolumeAndArea(length) {
+    if (typeof (length) !== 'number' || length < 0 || !Number.isInteger(length)) {
+        return "При вычислении произошла ошибка";
+    }
+
+    let volume = 0,
+        area = 0;
+
+    volume = length * length * length;
+    area = 6 * (length * length);
+
+    return `Объем куба: ${volume}, площадь всей поверхности: ${area}`;
 }
 
-console.log(calculateVolumeAndArea(15));
+console.log(calculateVolumeAndArea(5));
