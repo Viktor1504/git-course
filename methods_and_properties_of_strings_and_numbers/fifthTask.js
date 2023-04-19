93d17ded11f759c2a35c4e89ac7f4722975940e3
Viktor1504 <viktordenisevich6@gmail.com>
2023-04-20 00:36:29 +0300

    fifthTask finish 2

--- methods_and_properties_of_strings_and_numbers/fifthTask.js
+++ methods_and_properties_of_strings_and_numbers/fifthTask.js
@@ -1,21 +1,16 @@
 'use strict'
 
-function getTimeFromMinutes(num) {
-    if (typeof (num) !== 'number' || num < 0 || !Number.isInteger(num)) {
-        return 'Ошибка, проверьте данные';
+function findMaxNumber(num1, num2, num3, num4) {
+    let arr = [num1, num2, num3, num4];
+    let arrMax = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr < 4 || typeof (arr[i]) != 'number') return 0;
+
+        if (arrMax < arr[i]) {
+            arrMax = arr[i];
+        }
     }
-    let hours = Math.floor(num / 60);
-    let minutes = num % 60;
-    if (hours === 0) {
-        return `Это ${hours} часов и ${minutes} минут`;
-    }
-    if (hours % 10 === 1) {
-        return `Это ${hours} час и ${minutes} минут`;
-    } else if (hours % 10 >= 2 && hours % 10 <= 4) {
-        return `Это ${hours} часа и ${minutes} минут`;
-    } else {
-        return `Это ${hours} чаcов и ${minutes} минут`;
-    }
+    return arrMax;
 }
 
-console.log(getTimeFromMinutes(1260));
+console.log(findMaxNumber(8, 6, 1, 10));
