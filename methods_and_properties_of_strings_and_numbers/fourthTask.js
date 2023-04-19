b52f0d2a8db8dc9170dd4a97bd3073feac83bc58
Viktor1504 <viktordenisevich6@gmail.com>
2023-04-20 00:01:21 +0300

    fourhtTask finish

--- methods_and_properties_of_strings_and_numbers/fourthTask.js
+++ methods_and_properties_of_strings_and_numbers/fourthTask.js
@@ -4,16 +4,18 @@ function getTimeFromMinutes(num) {
     if (typeof (num) !== 'number' || num < 0 || !Number.isInteger(num)) {
         return 'Ошибка, проверьте данные';
     }
-    let remainder = num % 10;
     let hours = Math.floor(num / 60);
     let minutes = num % 60;
-    if (remainder === 1) {
+    if (hours === 0) {
+        return `Это ${hours} часов и ${minutes} минут`;
+    }
+    if (hours % 10 === 1) {
         return `Это ${hours} час и ${minutes} минут`;
-    } else if (remainder >= 2 && remainder <= 4) {
+    } else if (hours % 10 >= 2 && hours % 10 <= 4) {
         return `Это ${hours} часа и ${minutes} минут`;
     } else {
         return `Это ${hours} чаcов и ${minutes} минут`;
     }
 }
 
-console.log(getTimeFromMinutes(2));
+console.log(getTimeFromMinutes(1260));
