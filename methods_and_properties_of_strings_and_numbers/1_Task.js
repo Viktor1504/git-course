4d60f039845e1341c64152d49265f0f323ade33e
Viktor1504 <viktordenisevich6@gmail.com>
2023-04-20 21:28:41 +0300

    1_Tasks trim

--- methods_and_properties_of_strings_and_numbers/1_Task.js
+++ methods_and_properties_of_strings_and_numbers/1_Task.js
@@ -3,7 +3,7 @@
 let numberOfFilms;
 
 function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '').trim();
 
     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
