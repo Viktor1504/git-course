7a17f0828086254fcf42e328a127e760252c877e
Viktor1504 <viktordenisevich6@gmail.com>
2023-04-23 01:12:53 +0300

    4_Task объединение массивов

---
+++ objects/4_Task.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const someString = 'This is some strange string';
+
+function reverse(str) {
+    if (typeof str !== 'string') return 'Ошибка!';
+    return str.split('').reverse().join('');
+}
+
+console.log(reverse(someString));
