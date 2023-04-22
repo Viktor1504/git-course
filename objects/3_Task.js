3f8df486666076ebd6338f8cf62252905abe99e2
Viktor1504 <viktordenisevich6@gmail.com>
2023-04-23 00:39:30 +0300

    3_Task строка в обратном порядке с reverse()_new

--- objects/3_Task.js
+++ objects/3_Task.js
@@ -4,9 +4,7 @@ const someString = 'This is some strange string';
 
 function reverse(str) {
     if (typeof str !== 'string') return 'Ошибка!';
-    let arrSplit = str.split('');
-    arrSplit.reverse();
-    return arrSplit.join('');
+    return str.split('').reverse().join('');
 }
 
 console.log(reverse(someString));
