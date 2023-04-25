1b5a834ba6ce541c7881a2bfc6ce38c984954455
Viktor1504 <viktordenisevich6@gmail.com>
2023-04-25 22:35:07 +0300

    1_Task objects and arrays

---
+++ objects and arrays/1_Task.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+}
+
+function isBudgetEnough(data) {
+    let square = 0;
+    data.shops.forEach(item => {
+        square += item.length * item.width;
+    });
+    let volume = square * data.height;
+    return (volume * data.moneyPer1m3) <= data.budget ? 'Бюджета достаточно' : 'Бюджета недостаточно';
+}
+
+isBudgetEnough(shoppingMallData);
