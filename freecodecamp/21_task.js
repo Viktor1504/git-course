a802eeafcbce2762a63f64d4bab5695cbb6826f8
Viktor1504 <viktordenisevich6@gmail.com>
2023-06-11 14:37:04 +0300

    search in line several words - ignore case

--- freecodecamp/21_task.js
+++ freecodecamp/21_task.js
@@ -1,7 +1,7 @@
  'use strict'
 
  let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
- let waldoRegex = /Waldo|is/; // Change this line
+ let waldoRegex = /waldo/i; // Change this line   // i - игнорирование регистра
  let result = waldoRegex.test(waldoIsHiding);
 
  console.log(result);
