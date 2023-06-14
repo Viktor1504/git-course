666c87059d8eb353cd004c459a6d49495320f981
Viktor1504 <viktordenisevich6@gmail.com>
2023-06-14 23:41:29 +0300

    Match Whitespace

---
+++ freecodecamp/35_task.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g; // Change this line
+let result = sample.match(countWhiteSpace);
+
+console.log(result)
