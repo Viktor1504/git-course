6b49444237e47db57b95cd00e2eae15ca070b737
Viktor1504 <viktordenisevich6@gmail.com>
2023-06-20 23:36:48 +0300

    Use Capture Groups to Search and Replace

---
+++ freecodecamp/42_task.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let str = 'one two three';
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/;
+let replaceText = '$3 $2 $1';
+let result = str.replace(fixRegex, replaceText);
+
+console.log(result)
