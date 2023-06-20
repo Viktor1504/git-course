0f05663c8af23a4ef561713247e36081ab2e0a98
Viktor1504 <viktordenisevich6@gmail.com>
2023-06-20 23:44:00 +0300

    Remove Whitespace from Start and End

---
+++ freecodecamp/43_task.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+|\s+$/g; // Change this line
+let result = hello.replace(wsRegex, ""); // Change this line
+
+console.log(result)
