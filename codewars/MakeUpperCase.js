500d965e0937b0902abe4bf7a42a76eff868a41b
Viktor1504 <viktordenisevich6@gmail.com>
2023-06-30 21:50:13 +0300

    MakeUpperCase

---
+++ codewars/MakeUpperCase.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// function makeUpperCase(str) {
+//     return str.toUpperCase();
+// }
+
+
+
+let makeUpperCase = (str) => str.toUpperCase();
+
+console.log(makeUpperCase('hello'));
