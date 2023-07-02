cfde62a8e165c25cfb10d1011a9f6811b91ce535
Viktor1504 <viktordenisevich6@gmail.com>
2023-07-02 21:59:10 +0300

    Palindrome

--- codewars/Find the position letter.js
+++ codewars/Find the position letter.js
@@ -1,7 +1,7 @@
 'use strict'
 
 function position(letter) {
-    return `Position of alphabet: ${letter.charCodeAt() - 96}`;
+    return `Position of alphabet: ${letter.charCodeAt(0) - 96}`;
 }
 
 console.log(position('b'));
