6573968e7960038f4832465dd7d9f107297bff98
Viktor1504 <viktordenisevich6@gmail.com>
2023-07-05 00:17:04 +0300

    Summing a number digits

--- codewars/SentenceSmash.js
+++ codewars/SentenceSmash.js
@@ -1,7 +1,21 @@
 'use strict'
 
-function smash (words) {
-    return words.join(' ');
+// function sumDigits(number) {
+//     let str = Math.abs(number).toString().split('');
+//     let result = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         result += Number(str[i]);
+//     }
+//     return result;
+// }
+
+function sumDigits(number) {
+    let result = 0;
+    Math.abs(number).toString().split('').forEach(e => {
+        result += Number(e);
+    });
+
+    return result;
 }
 
-console.log(smash(['hello', 'world', 'this', 'is', 'great']));
+console.log(sumDigits(-343));
