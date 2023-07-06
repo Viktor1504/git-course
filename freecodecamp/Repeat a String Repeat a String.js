12be3cebc2e4881d5f7be9afa0c564cb8afebddb
Viktor1504 <viktordenisevich6@gmail.com>
2023-07-07 00:21:22 +0300

    Repeat a String Repeat a String

---
+++ freecodecamp/Repeat a String Repeat a String.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function repeatStringNumTimes(str, num) {
+    let result = '';
+    for (let i = 0; i < num; i++) {
+        result += str;
+    }
+    return result;
+}
+
+console.log(repeatStringNumTimes("abc", 3));
