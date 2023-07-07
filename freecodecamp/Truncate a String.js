900509faa596a4c2963323aee27ba9b032080ad2
Viktor1504 <viktordenisevich6@gmail.com>
2023-07-08 00:04:50 +0300

    Truncate a String

---
+++ freecodecamp/Truncate a String.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function truncateString(str, num) {
+    return str.length > num ? str.slice(0, num) + '...' : str;
+}
+
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 1));
