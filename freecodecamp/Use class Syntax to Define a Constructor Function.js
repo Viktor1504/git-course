828975f3ee9ce5bb11c7a4fdaa20ff9ee8bfa6d5
Viktor1504 <viktordenisevich6@gmail.com>
2023-07-30 19:22:48 +0300

    Use class Syntax to Define a Constructor Function

---
+++ freecodecamp/Use class Syntax to Define a Constructor Function.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class Vegetable {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
